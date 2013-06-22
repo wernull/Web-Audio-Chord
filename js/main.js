e46973a73f5c8e7692eacbc5e4342b383d8fba3f
Kyle Werner <kyle@wernull.com>
2013-06-21 23:08:06 -0400

    dial dragging with tweens

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
+var app = {
+	dial: $('.dial'),
+	site: $('html'),
+	init: function(){
+		//render background
+		app.setBgCheckerColors(203, 100, 50);
+		app.setMoveEvents();
+	},
+	setBgCheckerColors: function(h, s, l){
+		$('.color-bg').css('background-color', 'hsla('+h+', '+s+'%, '+l+'%, 1)');	
+	},
+	setMoveEvents: function(){
+		app.site.on('mousedown', function(e){app.enableMovement(e)});
+		app.site.on('mouseup', function(e){app.disableMovement(e)});
+	},
+	enableMovement: function(e){
+		TweenMax.to(app.dial, .2, {left: e.pageX, top: e.pageY});
+		app.site.on('mousemove', function(e){app.onMove(e)});
+	},
+	disableMovement: function(e){
+		app.site.off('mousemove');
+	},
+	onMove: function(e){
+		TweenMax.to(app.dial, .1, {left: e.pageX, top: e.pageY});
 
-function setBgCheckerColors(h, s, l){
-	$('.color-bg').css('background-color', 'hsla('+h+', '+s+'%, '+l+'%, 1)');
-	
+
+
+		//TweenMax.to($('.pointer-x'), 1.3, {rotation:-360, transformOrigin:"47% bottom", repeat:-1, ease:Linear.easeNone});
+		//TweenMax.to($('.pointer-y'), 1, {rotation:360, transformOrigin:"47% bottom", repeat:-1, ease:Linear.easeNone});
+	}
+
 }
 
-function init(){
-	//render background
-	setBgCheckerColors(203, 100, 50);
-	TweenMax.to($('.pointer-x'), 1.3, {rotation:-360, transformOrigin:"47% bottom", repeat:-1, ease:Linear.easeNone});
-	TweenMax.to($('.pointer-y'), 1, {rotation:360, transformOrigin:"47% bottom", repeat:-1, ease:Linear.easeNone});
-}
-
-$(init());
+$(app.init());
 
